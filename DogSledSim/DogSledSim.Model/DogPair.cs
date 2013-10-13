d286d4c92962632e5475149cab602fcf39436601
olavnybo@gmail.com <olavnybo@gmail.com>
2013-10-13 20:43:42 +0200

    Added adddog method

---
+++ DogSledSim/DogSledSim.Model/DogPair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSledSim.Model
+{
+    public class DogPair : Tuple<IDog, IDog>
+    {
+        public DogPair(IDog l, IDog r) : base(l, r)
+        {
+        }
+
+        public IEnumerable<IDog> Dogs { get { return new[] { Item1, Item2 }; } } 
+    }
+}
--- DogSledSim/DogSledSim.Model/DogSled.cs
+++ DogSledSim/DogSledSim.Model/DogSled.cs
@@ -8,12 +8,31 @@ namespace DogSledSim.Model
 {
     public class DogSled
     {
-        List<IDog> Dgs;
-        public DogSled(List<IDog> a)
+        IEnumerable<IDog> Dgs;
+
+        /// <summary>
+        /// Creates a dogsled and initialize the dog team
+        /// </summary>
+        /// <param name="a"></param>
+        public DogSled(List<ISledDog> a)
         {
             Dgs = a;
         }
 
+        /// <summary>
+        /// Adds a new pair of dogs to the list
+        /// </summary>
+        /// <param name="pair"></param>
+        public void AddPair(DogPair pair)
+        {
+            var l = new List<IDog>(Dgs);
+            l.AddRange(pair.Dogs);
+            Dgs = new List<IDog>(Dgs);
+        }
+
+        /// <summary>
+        /// Start the sled, tell all the dogs in the dog team to start running.
+        /// </summary>
         public void Start()
         {
             foreach (ISledDog d in Dgs)
